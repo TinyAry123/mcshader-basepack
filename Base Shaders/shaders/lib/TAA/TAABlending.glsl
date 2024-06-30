1b2f714bda3cc3c0374dcfff17a0847513f20548
TinyAry123 <aryamank495@gmail.com>
2024-06-30 13:06:22 +0800

    Optimised some code.

--- Base Shaders/shaders/lib/TAA/TAABlending.glsl
+++ Base Shaders/shaders/lib/TAA/TAABlending.glsl
@@ -1,19 +1,14 @@
-/*
-SMAA's optional temporal resolve method used. 
-*/
-
 vec4 TAABlending(out vec4 temporalColor, sampler2D currentColorTex, sampler2D previousColorTex, sampler2D velocityTex, vec2 uv, ivec2 screenSize) {
     ivec2 texelCoord = ivec2(floor(uv * screenSize));
 
-    vec2 velocity = -texelFetch(velocityTex, texelCoord, 0).xy;
+    vec2 velocity = texelFetch(velocityTex, texelCoord, 0).xy;
 
     vec4 current  = texelFetch(currentColorTex, texelCoord, 0);
-    vec4 previous = catmullRomTexture2D(previousColorTex, uv + velocity);
-
-    float delta  = abs(current.a - previous.a);
-    float weight = 0.5 * clamp(1.0 - sqrt(delta) * 30.0, 0.0, 1.0);
+    vec4 previous = catmullRomTexture2D(previousColorTex, uv - velocity);
     
     temporalColor = current;
 
-    return mix(current, previous, weight);
+    float weight = min(sqrt(abs(current.a - previous.a)) * 15.0 + 0.5, 1.0);
+
+    return mix(previous, current, weight);
 }

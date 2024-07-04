61dc74a2d923e5f244d9a4a8dcf5e9cfb72bb4f5
TinyAry123 <aryamank495@gmail.com>
2024-07-05 03:56:19 +0800

    Replaced instances of texelFetch where texelFetchOffset should be used.

--- Base Shaders/shaders/lib/SMAA/SMAANeighborhoodBlending.glsl
+++ Base Shaders/shaders/lib/SMAA/SMAANeighborhoodBlending.glsl
@@ -1,7 +1,7 @@
 void SMAANeighborhoodBlending(out vec4 color, sampler2D colorTex, sampler2D velocityTex, sampler2D blendTex, bool useTAA, vec2 uv, ivec2 screenSize) {
 	ivec2 texelCoord = ivec2(floor(uv * screenSize));
 
-	vec4 a = vec4(texelFetch(blendTex, texelCoord + ivec2( 1,  0), 0).w, texelFetch(blendTex, texelCoord + ivec2( 0,  1), 0).y, texelFetch(blendTex, texelCoord, 0).zx);
+	vec4 a = vec4(texelFetchOffset(blendTex, texelCoord, 0, ivec2( 1,  0)).w, texelFetchOffset(blendTex, texelCoord, 0, ivec2( 0,  1)).y, texelFetch(blendTex, texelCoord, 0).zx);
 
 	if (a.x + a.y + a.z + a.w >= 0.0000001) {
 		vec2 blendingCoord = max(a.x, a.z) > max(a.y, a.w) ? vec2(uv.x + mix(a.x, -a.z, a.z / (a.x + a.z)) / screenSize.x, uv.y) : vec2(uv.x, uv.y + mix(a.y, -a.w, a.w / (a.y + a.w)) / screenSize.y);

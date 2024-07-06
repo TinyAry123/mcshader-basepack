eb3b1b605cae1731270e19f9392b721cd702e2ff
TinyAry123 <aryamank495@gmail.com>
2024-07-06 22:04:03 +0800

    Optimised TAA reprojection and velocity calculation.

--- Base Shaders/shaders/lib/TAA/TAACalculateVelocity.glsl
+++ Base Shaders/shaders/lib/TAA/TAACalculateVelocity.glsl
@@ -8,22 +8,14 @@ used for TAA blending in the SMAA T2x pipeline.
 vec2 TAAReprojection(vec3 position) {
 	position = position * 2.0 - 1.0;
 
-	vec4 viewPositionPrevious =  gbufferProjectionInverse * vec4(position, 1.0);
-	viewPositionPrevious      /= viewPositionPrevious.w;
-	viewPositionPrevious      =  gbufferModelViewInverse * viewPositionPrevious;
+	vec4 viewPositionPrevious = gbufferProjectionInverse * vec4(position, 1.0);
+	viewPositionPrevious      = gbufferModelViewInverse * (viewPositionPrevious / viewPositionPrevious.w);
 
-    vec3 cameraOffset =  cameraPosition - previousCameraPosition;
-    cameraOffset      *= float(position.z > 0.56);
-
-	vec4 previousPosition = viewPositionPrevious + vec4(cameraOffset, 0.0);
-	previousPosition      = gbufferPreviousModelView * previousPosition;
-	previousPosition      = gbufferPreviousProjection * previousPosition;
+	vec4 previousPosition = gbufferPreviousProjection * (gbufferPreviousModelView * vec4(viewPositionPrevious.xyz + float(position.z > 0.56) * (cameraPosition - previousCameraPosition), viewPositionPrevious.w));
 	
     return previousPosition.xy / previousPosition.w * 0.5 + 0.5;
 }
 
 vec2 TAACalculateVelocity(sampler2D depthTex, vec2 uv, ivec2 screenSize) {
-    vec2 previousCoord = TAAReprojection(vec3(uv, texelFetch(depthTex, ivec2(floor(uv * screenSize)), 0).r));
-
-    return uv - previousCoord.xy;
+    return uv - TAAReprojection(vec3(uv, texelFetch(depthTex, ivec2(floor(uv * screenSize)), 0).r));
 }

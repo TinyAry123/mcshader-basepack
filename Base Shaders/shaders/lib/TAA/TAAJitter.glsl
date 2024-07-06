02e6ba9609cf3ad1d58e672f49aa79eaf86a2e93
TinyAry123 <aryamank495@gmail.com>
2024-07-06 22:31:26 +0800

    Modularised TAA jitter function.

--- Base Shaders/shaders/lib/TAA/TAAJitter.glsl
+++ Base Shaders/shaders/lib/TAA/TAAJitter.glsl
@@ -6,15 +6,13 @@ SMAA implementation, these offsets were given in GLSL coordinate system and acco
 HLSL coordinate system in the blending weight calculation stage. Since we are doing the blending weight
 calculation in our GLSL coordinate system, these offsets will have to be in HLSL coordinates; so the 
 y offsets are inverted here. 
-
-TAAJitter requires a custom frameMod2 uniform, set in shaders.properties as well as viewWidth and viewHeight uniforms. 
 */
 
-const vec2 jitterOffsets[2] = vec2[2](
+const vec2 TAAJitterOffsets[2] = vec2[2](
     vec2( 0.25,  0.25),
     vec2(-0.25, -0.25)
 );
 
-vec2 TAAJitter(vec2 coord, float w) {
-    return coord + w * jitterOffsets[frameMod2] / vec2(viewWidth, viewHeight);
+void TAAJitter(inout vec2 positionXY, float perspectiveScalar, int frameMod2, ivec2 screenSize) {
+    positionXY += perspectiveScalar * TAAJitterOffsets[frameMod2] / screenSize;
 }
